abde1d33ee9f1f7a106eacdeb90ccf88f6442b15
Antonin <antoningery@gmail.com>
2025-10-07 17:39:19 +0200

    base du front-end

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use('/api/auth', authRoutes);
 
