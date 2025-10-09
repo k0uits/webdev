1a344533f22f7e05678a1d07a0df0e7eab5f07fe
Antonin <antoningery@gmail.com>
2025-10-09 08:18:37 +0200

    EJS

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,20 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
-import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use('/api/auth', authRoutes);
 
-// Healthcheck
+app.get('/', (_req, res) => res.render('index'));
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
-// 404
 app.use((_req, res) => res.status(404).json({ message: 'Not Found' }));
 
-// Global error handler
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
   console.error(err);
   res.status(500).json({ message: 'Server error' });
