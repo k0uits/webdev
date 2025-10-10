b9bcce32b91587b0e257b040e8d17512408b3860
Antonin <antoningery@gmail.com>
2025-10-10 09:15:22 +0200

    Accueil

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from 'express';
+import path from 'path';
 import authRoutes from './routes/authRoutes';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, '../views'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/auth', authRoutes);
 
-app.get('/', (_req, res) => res.render('index'));
+app.get('/', (_req, res) => res.render('home'));
+app.get('/login', (_req, res) => res.render('login'));
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
 app.use((_req, res) => res.status(404).json({ message: 'Not Found' }));
-
 app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
   console.error(err);
   res.status(500).json({ message: 'Server error' });
