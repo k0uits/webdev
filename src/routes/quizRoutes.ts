8cf31e5dd1f6511ac28892d72168c948f37b759f
LEKhai <quangkhaile42@gmail.com>
2025-11-05 20:22:56 +0100

    myallquizsuppression

--- src/routes/quizRoutes.ts
+++ src/routes/quizRoutes.ts
@@ -4,7 +4,15 @@ import { ensureAuthenticated } from "../middleware/auth";
 
 const router = Router();
 
+router.get("/quizzes/mine", ensureAuthenticated, (req, res) => {
+  const anyReq: any = req;
+  res.render("myallquiz", { user: anyReq.user, mode: "mine" });
+});
 
+router.get("/quizzes/all", (req, res) => {
+  const anyReq: any = req;
+  res.render("myallquiz", { user: anyReq.user || null, mode: "all" });
+});
 
 // Page de création (formulaire)
 router.get("/quizzes/new", getCreatePage);
@@ -25,14 +33,5 @@ router.post("/quizzes",ensureAuthenticated , createQuiz);
 // suppression réservée aux connectés (logique fine dans deleteQuiz)
 router.delete("/quizzes/:id", ensureAuthenticated, deleteQuiz);
 
-router.get("/quizzes/mine", ensureAuthenticated, (req, res) => {
-  const anyReq: any = req;
-  res.render("myallquiz", { user: anyReq.user, mode: "mine" });
-});
-
-router.get("/quizzes/all", (req, res) => {
-  const anyReq: any = req;
-  res.render("myallquiz", { user: anyReq.user || null, mode: "all" });
-});
 
 export default router;
